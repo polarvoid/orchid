380c8ba2aaeaed021d9fc6211b86b950da4de021
Suven-p <pandeysuven@gmail.com>
2022-12-24 01:49:08 +0545

    Add homepage

---
+++ pages/app.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Link from 'next/link';
+
+const homepage = () => {
+    return (
+        <div className="flex flex-col h-full">
+            <Navbar />
+            <main className="flex grow">
+                <div className="mt-40 flex flex-col items-center grow">
+                    <p className="w-[40%] font-bold text-black text-7xl" >
+                        Create your own<br></br>
+                        beautiful brand
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur. Ornare ornare quam massa enim.
+                    </p>
+                    <Link href="/start" className="px-20 py-5 mt-5 text-white bg-[#454545CC] rounded-[56px]" >
+                        Get Started
+                    </Link>
+
+                </div>
+            </main>
+        </div>
+    );
+};
+
+export default homepage;
