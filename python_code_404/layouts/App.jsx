9294c637fad0e752de8915aae198335a8058e1a9
You-now-Who <yashyadav.sr@gmail.com>
2022-12-25 02:19:19 +0530

    Added my python code

---
+++ python_code_404/layouts/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+function App() {
+  return (
+    <>
+    {/* <Router> */}
+      <div data-theme="dark" className="container my3" id="main_container">
+        <h1 id="Title"></h1>
+        <h2 id="Description"></h2>
+      </div>
+    </>
+    );
+}
+
+export default App;
+
+//`npm run start` to run the server
