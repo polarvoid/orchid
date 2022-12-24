aa13e1d0f7b58439b1c2e527424990c14364a558
Alvin Philips <alvinphilips257@gmail.com>
2022-12-24 21:08:44 +0530

    Added Appwrite function

---
+++ appwrite/functions/Generate Template Project/src/index.py
@@ -0,0 +1,62 @@
+from appwrite.client import Client
+from appwrite.id import ID
+from appwrite.input_file import InputFile
+
+# You can remove imports of services you don't use
+from appwrite.services.account import Account
+from appwrite.services.avatars import Avatars
+from appwrite.services.databases import Databases
+from appwrite.services.functions import Functions
+from appwrite.services.health import Health
+from appwrite.services.locale import Locale
+from appwrite.services.storage import Storage
+from appwrite.services.teams import Teams
+from appwrite.services.users import Users
+
+"""
+  'req' variable has:
+    'headers' - object with request headers
+    'payload' - request body data as a string
+    'variables' - object with function variables
+
+  'res' variable has:
+    'send(text, status)' - function to return text response. Status code defaults to 200
+    'json(obj, status)' - function to return JSON response. Status code defaults to 200
+
+  If an error is thrown, a response with code 500 will be returned.
+"""
+
+def main(req, res):
+  client = Client()
+
+  # You can remove services you don't use
+  account = Account(client)
+  avatars = Avatars(client)
+  database = Databases(client)
+  functions = Functions(client)
+  storage = Storage(client)
+  health = Health(client)
+  locale = Locale(client)
+  teams = Teams(client)
+  users = Users(client)
+
+  bucket_id = '63a697019e70119538d3'
+
+  if not req.variables.get('APPWRITE_FUNCTION_ENDPOINT') or not req.variables.get('APPWRITE_FUNCTION_API_KEY'):
+    print('Environment variables are not set. Function cannot use Appwrite SDK.')
+  else:
+    (
+    client
+      .set_endpoint(req.variables.get('APPWRITE_FUNCTION_ENDPOINT', None))
+      .set_project(req.variables.get('APPWRITE_FUNCTION_PROJECT_ID', None))
+      .set_key(req.variables.get('APPWRITE_FUNCTION_API_KEY', None))
+      .set_self_signed(True)
+    )
+
+    result = storage.create_file(bucket_id, ID.unique(), InputFile.from_path('README.md'))
+    print(result)
+    return res.json(result)
+  
+  return res.json({
+    "areDevelopersAwesome": True,
+  })
